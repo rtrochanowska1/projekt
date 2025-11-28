0b3739cc849f4418b0ef2807351f5659bed55c13
Róża Trochanowska <rtrochanowska@st.swps.edu.pl>
2025-11-28 02:26:01 +0100

    added class Order to models.py, changed admin.py and settings.py)

--- sklep_internetowy/sklep/admin.py
+++ sklep_internetowy/sklep/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import (
+    Customer, Coffee, Producent, CoffeeTaste, Order)
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname", "email", "loyalty_points", "registration_date"]
+    readonly_fields = ('registration_date',) #tylko do odczytu
+    list_filter = ["registration_date"]
+    ordering = ('surname',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "customer", "date_ordered", "is_completed", "transaction_id"]
+    readonly_fields = ('date_ordered',) #tylko do odczytu
+    list_filter = ["is_completed", "date_ordered"]
+    ordering = ('-date_ordered',) 
+
+
+class CoffeeAdmin(admin.ModelAdmin):
+    list_display = ["name", "producent", "price", "type"]
+    list_filter = ["producent", "type"]
+    ordering = ('name',)
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Coffee, CoffeeAdmin)
+admin.site.register(Producent)
+admin.site.register(CoffeeTaste)
--- sklep_internetowy/sklep/models.py
+++ sklep_internetowy/sklep/models.py
@@ -33,7 +33,7 @@ class CoffeeTaste(models.Model):
     )
     coffee_acidity = models.IntegerField(
         help_text="Kwasowość kawy w skali od 1-3.",
-        validators=[MinValueValidator(1), MaxValueValidator(3)]
+        validators=[MinValueValidator(1), MaxValueValidator(3)] 
     )
 
     def __str__(self):
@@ -55,8 +55,47 @@ class Coffee(models.Model):
     type = models.CharField(max_length=1, choices=COFFEE_TYPES, default="Z")
     taste = models.ForeignKey(CoffeeTaste, null=True, blank=True, on_delete=models.SET_NULL)
     producent = models.ForeignKey(Producent, null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(
+        max_digits=5, #maksymalnie 5 cyfr
+        decimal_places=2, #2 miejsca po przecinku
+        help_text="Cena kawy w PLN."
+    )
 
     def __str__(self):
         return self.name
+    
+
+class Customer(models.Model):
+    """Model reprezentujący klienta sklepu."""
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    registration_date = models.DateField(auto_now_add=True, editable = False) 
+    loyalty_points = models.PositiveIntegerField(
+        default=0,
+        help_text="Aktualna liczba punktów lojalnościowych klienta."
+    )
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+    
+    class Meta:
+        ordering = ["surname"] #sortuj alfabetycznie po nazwisku
+
+
+class Order(models.Model):
+    """Model reprezentujący zamówienie klienta."""
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True, help_text="Klient, który złożył zamówienie.")
+    date_ordered = models.DateTimeField(auto_now_add=True) #automatyczna data złożenia zamówienia
+    is_completed = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        customer_surname = self.customer.surname
+        return f'Zamówienie nr {self.id} dla {customer_surname}'
+
+    class Meta:
+        ordering = ['-date_ordered'] #sortuj według najnowszego zamówienia
 
 
